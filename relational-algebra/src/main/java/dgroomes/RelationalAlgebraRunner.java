5af477c4d55601989e05e21adc80763d69c8c9eb
David Groomes <dwgroomes@gmail.com>
2023-07-24 21:15:22 -0500

    [`relational-algebra/`] Extract a DriverlessDataContext

--- relational-algebra/src/main/java/dgroomes/RelationalAlgebraRunner.java
+++ relational-algebra/src/main/java/dgroomes/RelationalAlgebraRunner.java
@@ -1,16 +1,14 @@
 package dgroomes;
 
-import org.apache.calcite.DataContext;
-import org.apache.calcite.adapter.java.JavaTypeFactory;
 import org.apache.calcite.adapter.java.ReflectiveSchema;
 import org.apache.calcite.interpreter.Interpreter;
-import org.apache.calcite.linq4j.QueryProvider;
+import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.JoinRelType;
 import org.apache.calcite.schema.SchemaPlus;
-import org.apache.calcite.tools.FrameworkConfig;
-import org.apache.calcite.tools.Frameworks;
-import org.apache.calcite.tools.RelBuilder;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.tools.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,12 +22,12 @@ public class RelationalAlgebraRunner {
 
     private static final Logger log = LoggerFactory.getLogger(RelationalAlgebraRunner.class);
 
-    /**
-     * Note: "oid" means "object ID". It's a unique identifier for that object type.
-     */
+    private FrameworkConfig frameworkConfig;
+    private SchemaPlus geographiesSchema;
+
     public static class City {
 
-        public final int oid;
+        public final int oid; // "oid" means "object ID". It's a unique identifier for the object.
         public final String name;
         public final String stateCode;
 
@@ -43,7 +41,6 @@ public class RelationalAlgebraRunner {
     public static class Zip {
         public final int zipCode;
         public final int population;
-
         public final int cityOid;
 
         public Zip(int zipCode, int population, int cityOid) {
@@ -76,9 +73,11 @@ public class RelationalAlgebraRunner {
     }
 
     public static void main(String[] args) {
-        log.info("Let's learn the Apache Calcite relational algebra API by summing up a sample of ZIP code population data by their city.");
+        log.info("Let's learn the Apache Calcite relational algebra API!");
+        new RelationalAlgebraRunner().run();
+    }
 
-        SchemaPlus geographiesSchema;
+    public void run() {
         {
             // Let's create the schema objects and wire in some data. For convenience, let's use a POJO-based data set
             // that represents ZIP codes and cities ("geographies").
@@ -87,12 +86,18 @@ public class RelationalAlgebraRunner {
             geographiesSchema = rootSchema.add("geographies", reflectiveSchema);
         }
 
+        frameworkConfig = Frameworks.newConfigBuilder().defaultSchema(geographiesSchema).build();
 
-        FrameworkConfig config = Frameworks.newConfigBuilder().defaultSchema(geographiesSchema).build();
+        cityPop();
+    }
 
+    /**
+     * Sum up ZIP code population data to their city.
+     */
+    private void cityPop() {
         // Let's build a relational algebra expression directly. While the official Calcite codebase generally tests
         // with SQL, there are some examples that use the relational algebra API, like this: https://github.com/apache/calcite/blob/3c5345c988e43622e7dd1e8197972c7664514da1/core/src/test/java/org/apache/calcite/examples/RelBuilderExample.java#L31
-        RelBuilder builder = RelBuilder.create(config);
+        RelBuilder builder = RelBuilder.create(frameworkConfig);
         RelNode node = builder
                 .scan("zips")
                 .scan("cities")
@@ -101,7 +106,7 @@ public class RelationalAlgebraRunner {
                                 builder.field(2, 0, "cityOid"),
                                 builder.field(2, 1, "oid")))
                 // How can we show the city name instead of the city OID? I want to still group by the OID because
-                // that's the right identifier but I want to present the city name. I had an earlier tactic of writing
+                // that's the right identifier, but I want to present the city name. I had an earlier tactic of writing
                 // SQL (which I know how to write) and then converting it to a relational algebra expression (which I'm
                 // still learning). I should do that.
                 .aggregate(builder.groupKey("cityOid"),
@@ -109,7 +114,7 @@ public class RelationalAlgebraRunner {
                 .project(builder.field(0), builder.field("city_population"))
                 .build();
 
-        GeographiesDataContext dataContext = new GeographiesDataContext(geographiesSchema, node);
+        DriverlessDataContext dataContext = new DriverlessDataContext(geographiesSchema, node);
 
         try (Interpreter interpreter = new Interpreter(dataContext, node)) {
             interpreter.forEach(row -> {
@@ -122,40 +127,25 @@ public class RelationalAlgebraRunner {
     }
 
     /**
-     * This part of the Calcite API is a bit odd and circuitous (I think because Calcite is not often used without
-     * JDBC). We need to create an instance of a {@link DataContext} and interestingly I think our only option is to
-     * implement the interface directly (and eat the cost of unused boilerplate methods).
-     * <p>
-     * This is adapted from a <a href="https://github.com/apache/calcite/blob/3c5345c988e43622e7dd1e8197972c7664514da1/core/src/test/java/org/apache/calcite/test/InterpreterTest.java#L82">Calcite test suite</a>.
-     * I'm so glad I found this otherwise I was about to give up (I had kind of given up before finding this).
+     * This is a bit off-topic but I've found it difficult to write working relational expressions. By contrast, I can
+     * figure out how to write working SQL queries. Conveniently, you can convert a SQL query to a relational text
+     * expression and then I have a better chance at writing the expression in Java.
      */
-    public static class GeographiesDataContext implements DataContext {
-        private final SchemaPlus rootSchema;
-        private final JavaTypeFactory typeFactory;
+    private void examineSqlAsRelationalExpression() {
+        // SQL Query
+        String sql = "SELECT e.name, d.name FROM emps e inner join depts d on e.deptno = d.deptno";
 
-        GeographiesDataContext(SchemaPlus rootSchema, RelNode rel) {
-            this.rootSchema = rootSchema;
-            this.typeFactory = (JavaTypeFactory) rel.getCluster().getTypeFactory();
-        }
+        // Creating planner with default settings (what is a planner?)
+        Planner planner = Frameworks.getPlanner(frameworkConfig);
 
-        @Override
-        public SchemaPlus getRootSchema() {
-            return rootSchema;
-        }
-
-        @Override
-        public JavaTypeFactory getTypeFactory() {
-            return typeFactory;
-        }
-
-        @Override
-        public QueryProvider getQueryProvider() {
-            return null;
-        }
-
-        @Override
-        public Object get(String name) {
-            return null;
+        RelNode node;
+        try {
+            SqlNode parsedSql = planner.parse(sql);
+            SqlNode validatedSql = planner.validate(parsedSql);
+            node = planner.rel(validatedSql).rel;
+            log.info(RelOptUtil.toString(node));
+        } catch (SqlParseException | ValidationException | RelConversionException e) {
+            throw new RuntimeException(e);
         }
     }
 
